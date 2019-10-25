47ea1b6ed444f7585efab30e3ebd124e4c536cd6
Sxmmie <samuelumohjnr@gmail..com>
2019-10-25 13:46:17 +0100

    Added user model properties

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=true)
+    is_active = models.BooleanField(default=true)
+    is_staff = models.BooleanField(default=true)
+
